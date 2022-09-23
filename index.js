781d1ed91d40343e2a617a1361092409a896c469
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-09-23 23:02:12 +0200

    sensors drawn and attached (yellow lines)

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
 //set which lane is the car on
 const car = new Car(road.getLaneCenter(1), 100, 30, 50);
-car.draw(ctx);
 
 animate();
 
