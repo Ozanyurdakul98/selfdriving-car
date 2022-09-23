0c6b8c3b4ab319d5e5005eae3081ce10c57fee61
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-09-23 23:29:38 +0200

    add raycasting

--- components/car/sensors.js
+++ components/car/sensors.js
@@ -1,19 +1,54 @@
 class Sensor {
   constructor(car) {
     this.car = car;
-    this.rayCount = 4;
-    this.rayLength = 100;
-    this.raySpread = Math.PI / 4;
+    this.rayCount = 10;
+    this.rayLength = 150;
+    this.raySpread = Math.PI / 2;
 
     this.rays = [];
+    this.readings = [];
   }
 
-  update() {
+  update(roadBoarders) {
+    this.#castRays();
+    this.readings = [];
+    for (let i = 0; i < this.rays.length; i++) {
+      this.readings.push(this.#getReading(this.rays[i], roadBoarders));
+    }
+  }
+
+  #getReading(ray, roadBoarders) {
+    let touches = [];
+    for (let i = 0; i < roadBoarders.length; i++) {
+      const touch = getIntersection(
+        ray[0],
+        ray[1],
+        roadBoarders[i][0],
+        roadBoarders[i][1]
+      );
+      if (touch) {
+        touches.push(touch);
+      }
+    }
+    if (touches.length == 0) {
+      return null;
+    } else {
+      const offsets = touches.map((e) => e.offset);
+      //setting that we know just max 1 touch of the arrays
+      const minOffset = Math.min(...offsets);
+      return touches.find((e) => e.offset == minOffset);
+    }
+  }
+
+  #castRays() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
       const rayAngle =
-        lerp(this.raySpread / 2, -this.raySpread / 2, i / (this.rayCount - 1)) +
-        this.car.angle;
+        lerp(
+          this.raySpread / 2,
+          -this.raySpread / 2,
+          this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+        ) + this.car.angle;
 
       const start = { x: this.car.x, y: this.car.y };
       const end = {
@@ -23,13 +58,27 @@ class Sensor {
       this.rays.push([start, end]);
     }
   }
+
   draw(ctx) {
     for (let i = 0; i < this.rayCount; i++) {
+      let end = this.rays[i][1];
+      if (this.readings[i]) {
+        end = this.readings[i];
+      }
+
       ctx.beginPath();
       ctx.lineWidth = 2;
       ctx.strokeStyle = "yellow";
       ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
-      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.lineTo(end.x, end.y);
+      ctx.stroke();
+
+      //draw line continiosly after touching border
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "red";
+      ctx.moveTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.lineTo(end.x, end.y);
       ctx.stroke();
     }
   }
